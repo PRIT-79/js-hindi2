d06a53904c57ee0ef36c1bf329dc725d615f3fd0
PRIT-79 <chavanpritti709@gmail.com>
2025-02-13 13:41:15 +0000

    date_time

---
+++ .devcontainer/basics/date_time.js
@@ -0,0 +1,38 @@
+//dates
+
+let mydate = new Date()
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof(mydate));
+
+// let myCreatedDate = new Date(2025,0,23)
+// console.log(myCreatedDate.toDateString());
+
+
+let Mycreatedate =new Date("01-12-2024")
+// console.log(Mycreatedate.toLocaleDateString());
+
+
+let myTimeStamp =Date.now()
+// console.log(myTimeStamp);
+// console.log(Mycreatedate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth());
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    
+})
+
+
+
+
+
+
+
+
+
