7d4ff1f5433a32660f6229ac969efdae7d75b578
PRIT-79 <chavanpritti709@gmail.com>
2025-02-13 11:06:57 +0000

    datatypesummary

---
+++ .devcontainer/basics/datatype_summary.js
@@ -0,0 +1,47 @@
+// primitive - string,number,boolean,null,undefined,symbol,BigInt
+
+// reference(non-primitive)-array,objects,functions
+
+// const score =100
+// const scoreValue = 100.3
+
+// const isLoggedIn =false
+// const outsideTemp = null
+// let userEmail;
+
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 345654356754326n
+console.log(typeof(bigNumber));
+
+
+// const heros=["shaktiman","naagraj","doga"]
+
+// let myobj ={
+//     name:'priti'
+//     subject:javascript,
+// }
+
+/*
+Return type of variables in javascript
+
+1] Primitive Datatypes
+    Number =>number
+    String =>string
+    boolean=>boolean
+    null =>object
+    undefined=>undefined
+    symbol=>symbol
+    bigInt =>undefined
+
+2]Non-Primitive Datatype
+
+Arrays=>object
+object=>object
+function=>function
+
+*/ 
