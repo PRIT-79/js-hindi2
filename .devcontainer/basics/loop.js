518ad4fba83f231b841030d16f3d7af6a6bd0c4a
PRIT-79 <chavanpritti709@gmail.com>
2025-02-28 10:46:51 +0000

    loop

---
+++ .devcontainer/basics/loop.js
@@ -0,0 +1,44 @@
+// for of
+
+const arr = ["priti","pragati","sonali","radhika"]
+
+for (const element of arr) {
+
+    // console.log(element);
+    
+}
+
+const arr1 = [1,2,3,4,5]
+
+for (const values of arr1) {
+    
+    // console.log(values);
+    
+}
+
+const greeting = "hello world"
+
+for (const greet of greeting) {
+    
+    // console.log(greet);
+    
+}
+const map =new Map()
+map.set('IN',"India")
+map.set('USA',"United states of America")
+map.set('Fr',"France")
+map.set('IN',"India")
+
+
+// console.log(map); //map gives us unique value it cannot return matching  record again and again
+
+// for (const [key,value] of map) {
+    
+//     // console.log(key, ':', value);
+
+    
+// }
+
+
+
+
---
+++ .devcontainer/basics/loop2.js
@@ -0,0 +1,19 @@
+// const myObject = {
+//     js :'javascript',
+//     cpp : 'c++',
+//     rb:'rubby',
+//     swift :'swift by apple'
+// }
+
+// for (const key in myObject) {
+//     // console.log(`${key} - ${myObject[key]}`);
+    
+// }
+
+const programming = ["js","rb","py","php","java"]
+
+for (const key in programming) {
+    
+    console.log(key);
+    
+}
